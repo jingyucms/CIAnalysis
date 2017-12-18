09ab54bb79a1fde16c0bbde70da0519f7d236f30
jschulte <jschulte@cern.ch>
2017-12-17 19:48:06 -0500

    update mass binning for mass binned limits

--- signalFitting.py
+++ signalFitting.py
@@ -51,9 +51,8 @@ def main():
 	massPlotUp = getPlot("massPlotUp")
 	massPlotDown = getPlot("massPlotDown")
 	massPlotWeighted = getPlot("massPlotWeighted")
-	xMax = 3500
-	xMax = 3500
-	massBins = [400,500,700,1100,1900,xMax]
+	xMax = 5000
+	massBins = [400,500,700,1100,1900,3500,xMax]
 	massCuts = [400,500,600,700,800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,3000,3100,3200,3300,3400,3500]
 	
 	signalYields = {}
