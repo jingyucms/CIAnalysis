f43c699ea093651de4189b5f8517093f061f85c5
jschulte <jschulte@cern.ch>
2018-01-25 15:00:28 -0500

    asdfasdf

--- limitInputs.py
+++ limitInputs.py
@@ -7,12 +7,39 @@ from copy import deepcopy
 from helpers import *
 from defs import getPlot, Backgrounds, Signals, Data
 
+tableTemplate = '''
+\\begin{table}
+\\begin{center}
+\\begin{tabular}{c|ccccc}
+ Signal Model & \multicolumn{5}{c}{BB category} \\\\ \\hline
+ & 400-500 & 500-700 & 700-1100 & 1100-1900 & 1900-3500 \\\\ \\hline
+ %s 
+ %s
+ %s
+ %s
+ %s
+ %s 
+ & \multicolumn{5}{c}{BE category} \\\\ \\hline
+ & 400-500 & 500-700 & 700-1100 & 1100-1900 & 1900-3500 \\\\ \\hline
+ %s 
+ %s
+ %s
+ %s
+ %s
+ %s 
+\end{tabular}
+\end{center}
+\end{table}
+'''
+lineTemplate = " %s & %.3f  & %.3f & %.3f & %.3f & %.3f  \\\\"
+
 def main():
 	### for data
 
 	
 	histos = ["BB","BE"]
-	labels = ["dimuon_BB","dimuon_BE"]
+	#~ labels = ["dimuon_Moriond2017_BB","dimuon_Moriond2017_BE"]
+	labels = ["dimuon_Moriond2017_BE"]
 	
 	lambdas = [10,16,22,28,34]
 	models = ["ConLL","ConLR","ConRR","DesLL","DesLR","DesRR"]
@@ -24,6 +51,8 @@ def main():
 	massPlotDown = getPlot("massPlotDown")
 	massPlotWeighted = getPlot("massPlotWeighted")
 
+	massBins = [400,500,700,1100,1900,3500]
+	signalYields = {}
 
 
 	for i, histo in enumerate(histos):
@@ -36,9 +65,10 @@ def main():
 				other = Process(getattr(Backgrounds,"Other"))				
 				name = "CITo2Mu_Lam%dTeV%s"%(l,model)
 				signal = Process(getattr(Signals,name))
+				signalYields["%s_%s"%(name,label)] = {}
 			
-				name = "CITo2Mu_Lam100kTeV%s"%(model)
-				signalDY = Process(getattr(Signals,name))
+				nameDY = "CITo2Mu_Lam100kTeV%s"%(model)
+				signalDY = Process(getattr(Signals,nameDY))
 			
 				fResult = TFile("inputsCI_%s_%dTeV_%s.root"%(label,l,model),"RECREATE")	
 				
@@ -74,12 +104,13 @@ def main():
 				sigHistScaleDown.Add(sigHistScaleDownDY,-1)
 				sigHistWeighted.Add(sigHistWeightedDY,-1)
 	
-				hist = data.loadHistogramProjected(massPlot, bins[i])
+				hist = deepcopy(data.loadHistogramProjected(massPlot, bins[i]))
 				if "_BE" in label:
 					hist.Add(deepcopy(data.loadHistogramProjected(massPlot, 10)))				
 				hist.SetName("dataHist_%s" %label)
 
 				dyHist = deepcopy(drellyan.loadHistogramProjected(massPlot, bins[i]))
+				print dyHist.Integral()
 				dyHistSmear = deepcopy(drellyan.loadHistogramProjected(massPlotSmeared, bins[i]))
 				dyHistScaleUp = deepcopy(drellyan.loadHistogramProjected(massPlotUp, bins[i]))
 				dyHistScaleDown = deepcopy(drellyan.loadHistogramProjected(massPlotDown, bins[i]))
@@ -129,7 +160,11 @@ def main():
 				jetHist = fJets.Get("TotalJets")
 				jetHist.SetName("bkgHistJets_%s"%label)				
 				jetHist.SetDirectory(fResult)				
-				
+				print hist.Integral(hist.FindBin(400),hist.FindBin(500-0.01))
+				print dyHist.Integral(dyHist.FindBin(400),dyHist.FindBin(500-0.01)) + otherHist.Integral(otherHist.FindBin(400),otherHist.FindBin(500-0.01)) + jetHist.Integral(jetHist.FindBin(400),jetHist.FindBin(500-0.01))
+				print dyHist.Integral(dyHist.FindBin(400),dyHist.FindBin(500-0.01)) 
+				print otherHist.Integral(otherHist.FindBin(400),otherHist.FindBin(500-0.01)) 
+				print jetHist.Integral(jetHist.FindBin(400),jetHist.FindBin(500-0.01))		
 
 				sigHist.SetDirectory(fResult)
 				sigHistSmear.SetDirectory(fResult)
@@ -147,12 +182,27 @@ def main():
 				otherHistScaleDown.SetDirectory(fResult)
 				otherHistWeighted.SetDirectory(fResult)
 				hist.SetDirectory(fResult)
-
-					
+				for index, mass in enumerate(massBins):
+					if index < len(massBins)-1:
+						
+						signalYields["%s_%s"%(name,label)][str(index)] = sigHist.Integral(sigHist.FindBin(mass),sigHist.FindBin(massBins[index+1]-0.01))
 									
 				fResult.Write()
 				fResult.Close()
+				
 
-		
+	for l in lambdas:
+		lineTemplatesBB = []
+		lineTemplatesBE = []
+		for model in models:
+			
+			labelBB = labels[0]
+			labelBE = labels[1]
+
+			name = "CITo2Mu_Lam%dTeV%s"%(l,model)
+				
+			lineTemplatesBB.append(lineTemplate%("%dTeV %s"%(l,model),signalYields["%s_%s"%(name,labelBB)]["0"],signalYields["%s_%s"%(name,labelBB)]["1"],signalYields["%s_%s"%(name,labelBB)]["2"],signalYields["%s_%s"%(name,labelBB)]["3"],signalYields["%s_%s"%(name,labelBB)]["4"]))
+			lineTemplatesBE.append(lineTemplate%("%dTeV %s"%(l,model),signalYields["%s_%s"%(name,labelBE)]["0"],signalYields["%s_%s"%(name,labelBE)]["1"],signalYields["%s_%s"%(name,labelBE)]["2"],signalYields["%s_%s"%(name,labelBE)]["3"],signalYields["%s_%s"%(name,labelBE)]["4"]))
+		print tableTemplate%(lineTemplatesBB[0],lineTemplatesBB[1],lineTemplatesBB[2],lineTemplatesBB[3],lineTemplatesBB[4],lineTemplatesBB[5],lineTemplatesBE[0],lineTemplatesBE[1],lineTemplatesBE[2],lineTemplatesBE[3],lineTemplatesBE[4],lineTemplatesBE[5])								
 		
 main()
